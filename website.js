da3f0c93e404764909393fb2e492a9841f7d997c
nicksmith105 <30782766+nicksmith105@users.noreply.github.com>
2019-03-14 13:37:51 -0700

    initial commit

---
+++ website.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+  
+var path = __dirname + "/views/";
+  
+app.use("/",router);
+  
+router.get("/",function(req, res){
+  res.sendFile(path + "index.html");
+});
+  
+router.get("/scoreboard",function(req, res){
+  res.sendFile(path + "scoreboard.html");});
+  
+router.get("/game",function(req, res){
+  res.sendFile(path + "game.html");
+});
+  
+app.use("*",function(req, res){
+  res.send("Error 404: Not Found!");
+});
+  
+app.listen(3000,function(){
+  console.log("Server running at Port 3000");
+});
